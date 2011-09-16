63dee47ca85966350015299af6a7160f4ff351da
Junwei Ren <renjwei@gmail.com>
2011-09-16 08:00:03 +0800

    commit first time to create the project. can build but not run correctly

---
+++ src/com/itcast/ui/UpdateWeiboActivity.java
@@ -0,0 +1,19 @@
+package com.itcast.ui;
+
+import com.itcast.logic.IWeiboActivity;
+
+public class UpdateWeiboActivity implements IWeiboActivity{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void refresh(Object... param) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
